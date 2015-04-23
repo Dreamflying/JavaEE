a373473201e220420614bab3dff4450624643b8e
liyuan <1522651962@qq.com>
2015-04-23 16:30:39 +0800

    ant test

---
+++ AntTest/src/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by liyuan on 2015/4/23.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
